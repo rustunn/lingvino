27a478dbef383685b8ea3b79517cd1364fe4a7d4
rustunn <nikolay.ayrapetov@gmail.com>
2016-05-21 10:25:48 -0700

    updates

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './components/Home';
+import { sync } from 'vuex-router-sync';
+import store from './vuex/store';
 Vue.use(Router);
 
 /* eslint-disable no-new */
 const router = new Router();
+sync(store, router);
 const App = Vue.extend({});
 
 router.map({
